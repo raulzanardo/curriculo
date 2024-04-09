8b8f2f3d137cf737f1e1e77b185b1cb95d159f4b
raul <r.zanardo@pensenova.com>
2024-04-09 11:24:56 -0300

    alteração do tipo de arquivo do CustomIcon

--- lib/widgets/widget_custom_icon.dart
+++ lib/widgets/widget_custom_icon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-//TODO mudar pra SVG
 class WidgetCustomIcon extends StatefulWidget {
   String iconName;
 
@@ -14,20 +14,14 @@ class WidgetCustomIcon extends StatefulWidget {
 }
 
 class _WidgetCustomIconState extends State<WidgetCustomIcon> {
-
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SvgPicture.asset(
       width: 25,
       height: 25,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-
-          image: (Theme.of(context).brightness == Brightness.dark ) ? AssetImage('assets/images/icons/icon_${widget.iconName}_light.png') :  AssetImage('assets/images/icons/icon_${widget.iconName}_dark.png'),
-        ),
-      ),
+      'assets/images/icons/icon_${widget.iconName}_dark.svg',
+      //TODO verificar outro método para troca de cor do SVG
+      color: (Theme.of(context).brightness == Brightness.dark )  ? Colors.white : Colors.black,
     );
   }
 }
